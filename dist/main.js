43df7de20aaffd5ad6a53c7f385e93e18b7101ed
leandrolol <leandrofaustiinoo@hotmail.com>
2024-06-25 23:15:32 -0300

    ES6

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+const alunos = ['Leandro', 'João', 'Paulo', 'Joaquim'];
+const notas = ['10', '7', '5', '9'];
+const alunosComNotas = alunos.map((nome, indice) => ({
+  nome: nome,
+  nota: notas[indice]
+}));
+console.log(alunosComNotas);
+const filtraAlunosAprovados = aluno => aluno.nota >= 6;
+const alunosAprovados = alunosComNotas.filter(filtraAlunosAprovados);
+console.log(alunosAprovados);
